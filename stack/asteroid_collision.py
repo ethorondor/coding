e04b0cee9bef7ecfd975383d9898da719631aefd
ethorondor <ethorondor@gmail.con>
2024-04-13 14:35:59 +0000

    master

--- stack/asteroid_collision.py
+++ stack/asteroid_collision.py
@@ -22,3 +22,23 @@ class Solutions:
 sln = Solutions()
 sln.collision(asteroids)
 # %%
+class Solutions:
+    def collision(self, asteroids):
+        stack = []
+        for a in asteroids:
+            while stack and a < 0 and stack[-1] > 0:
+                b = stack.pop()
+                if abs(a) < abs(b):
+                    a = b
+                elif abs(a) > abs(b):
+                    continue
+                else:
+                    a = 0
+            if a:
+                stack.append(a)
+        return stack
+
+asteroids = [-2,-1,1,2]
+sln = Solutions()
+sln.collision(asteroids)    
+# %%
